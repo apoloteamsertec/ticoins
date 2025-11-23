3a2c1c681ec0fa7c31b3908c465e2af4e33a13e5
apoloteamsertec <apoloteamsertec@gmail.com>
2025-11-23 20:55:53 -0300

    Add files via upload

--- auth/login.php
+++ auth/login.php
@@ -30,6 +30,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 <html lang="es">
 <head>
 <meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Panel Admin</title>
 <link rel="stylesheet" href="../assets/styles.css">
 <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600&display=swap" rel="stylesheet">
